0164f734140f3068b54fdf877e49293206ab1b95
SY <serkanyetim@yahoo.com>
2019-11-09 20:41:57 +0300

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author serkan
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World");
+
+	}
+
+}
